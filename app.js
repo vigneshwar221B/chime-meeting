b54adb4b8725fdb3b719f9503a52749db3a97b17
vigneshwar <vigneshwar221b@gmail.com>
2022-06-19 18:11:47 +0530

    server: setting up aws chime

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+const AWS = require("aws-sdk");
 const express = require("express");
+const { v4: uuid } = require("uuid");
+
 const app = express();
 
 const dotenv = require("dotenv");
@@ -7,9 +10,31 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/static"));
+const meetingTable = {};
+
+const chime = new AWS.Chime({ region: "us-east-1" });
+chime.endpoint = new AWS.Endpoint("https://service.chime.aws.amazon.com");
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/create-meeting", async (req, res) => {
+  const meetingResponse = await chime
+    .createMeeting({
+      ClientRequestToken: uuid(),
+      MediaRegion: "us-west-2", // Specify the region in which to create the meeting.
+    })
+    .promise();
+
+  const attendeeResponse = await chime
+    .createAttendee({
+      MeetingId: meetingResponse.Meeting.MeetingId,
+      ExternalUserId: uuid(), // Link the attendee to an identity managed by your application.
+    })
+    .promise();
+    
+  res.send({ meetingResponse, attendeeResponse });
 });
 
 app.listen(port, () => {
