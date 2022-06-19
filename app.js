7f3b718031330c93de50765094998d83f71803e1
vigneshwar <vigneshwar221b@gmail.com>
2022-06-19 17:49:00 +0530

    simple express server

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port http://localhost:${port}`);
+});
