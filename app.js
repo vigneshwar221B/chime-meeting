acc836d8e26eaef70a3a98e7ac0ed03f997dd204
vigneshwar <vigneshwar221b@gmail.com>
2022-06-20 14:34:40 +0530

    added the other endpoints

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { v4: uuid } = require("uuid");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { chime, getClientForMeeting, log } = require("./helper");
+const { chime, getClientForMeeting, log, S3_ARN } = require("./helper");
 
 const app = express();
 app.use(express.static(__dirname + "/static"));
@@ -17,6 +17,7 @@ app.get("/get-all-meetings", (req, res) => {
   res.json(meetingTable);
 });
 
+//end points
 app.post("/join", async (req, res) => {
   log(req.query);
 
@@ -61,6 +62,66 @@ app.post("/join", async (req, res) => {
   res.json(response);
 });
 
+app.post("/end", async (req, res) => {
+  let client = getClientForMeeting(meetingTable[req.query.title]);
+
+  await client
+    .deleteMeeting({
+      MeetingId: meetingTable[req.query.title].Meeting.MeetingId,
+    })
+    .promise();
+  res.send(`meeting ${req.query.title} has been ended`);
+});
+
+app.post("/remove-attendee", async (req, res) => {
+  if (!req.query.title || !req.query.attendeeId) {
+    res.status(400).send("need params: title and attendeeId");
+  }
+  let client = getClientForMeeting(meetingTable[req.query.title]);
+  const meeting = meetingTable[req.query.title];
+
+  await client
+    .deleteAttendee({
+      MeetingId: meeting.Meeting.MeetingId,
+      AttendeeId: req.query.attendeeId,
+    })
+    .promise();
+
+  res.send(`Attendee ${req.query.attendeeId} has been removed`);
+});
+
+app.post("/start-capture", async (req, res) => {
+  if (!S3_ARN) {
+    res.status(400).send("media capture is not available without a S3.");
+  }
+  const callerInfo = await sts.getCallerIdentity().promise();
+  const pipelineInfo = await chime
+    .createMediaCapturePipeline({
+      SourceType: "ChimeSdkMeeting",
+      SourceArn: `arn:aws:chime::${callerInfo.Account}:meeting:${
+        meetingTable[req.query.title].Meeting.MeetingId
+      }`,
+      SinkType: "S3Bucket",
+      SinkArn: S3_ARN,
+    })
+    .promise();
+  meetingTable[req.query.title].Capture = pipelineInfo.MediaCapturePipeline;
+  res.send("media capture is active now");
+});
+
+app.post("/end-capture", async (req, res) => {
+  if (!S3_ARN) {
+    res.status(400).send("media capture is not available without a S3.");
+  }
+  const pipelineInfo = meetingTable[req.query.title].Capture;
+  await chime
+    .deleteMediaCapturePipeline({
+      MediaPipelineId: pipelineInfo.MediaPipelineId,
+    })
+    .promise();
+  meetingTable[req.query.title].Capture = undefined;
+  res.send("media capture has been stopped");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`http://localhost:${port}`));
